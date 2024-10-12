5957e7f0427452f869e1be4d305b46a270b1f1de
BiancaDavey <60249994+BiancaDavey@users.noreply.github.com>
2024-10-12 20:20:55 +0200

    Disable header nav button for current page

--- react/src/components/Header.tsx
+++ react/src/components/Header.tsx
@@ -5,6 +5,36 @@ export default function Header() {
   const navigate = useNavigate()
   const location = useLocation()
   
+  const getButtons = () => {
+    if (location.pathname === '/') {
+      return (
+        <div className={style['header-items']}>
+          <button className={style['item-current']} onClick={() => navigate('/')}>Games</button>
+          <button className={style.item} onClick={() => navigate('projects')}>Projects</button>
+          <button className={style.item} onClick={() => navigate('about')}>About</button>
+      </div>
+      )
+    }
+    if (location.pathname === '/projects') {
+      return (
+        <div className={style['header-items']}>
+          <button className={style.item} onClick={() => navigate('/')}>Games</button>
+          <button className={style['item-current']} onClick={() => navigate('projects')}>Projects</button>
+          <button className={style.item} onClick={() => navigate('about')}>About</button>
+      </div>
+      )
+    }
+    if (location.pathname === '/about') {
+      return (
+        <div className={style['header-items']}>
+          <button className={style.item} onClick={() => navigate('/')}>Games</button>
+          <button className={style.item} onClick={() => navigate('projects')}>Projects</button>
+          <button className={style['item-current']} onClick={() => navigate('about')}>About</button>
+        </div>
+      )
+    }
+  }
+
   const getTitle = () => {
     if (location.pathname === '/') {
       return (
@@ -41,31 +71,13 @@ export default function Header() {
         </>
       )
     }
-    if (location.pathname === '/gomoku-game') {
-      return (
-        <>
-        Gomoku
-        </>
-      )
-    }
-    if (location.pathname === '/games') {
-      return (
-        <>
-        Gomoku
-        </>
-      )
-    }
   }
 
   return (
     <header className={style.header}>
       <div className={style.container}>
       <Link className={style.title} to="/">{getTitle()}</Link>
-        <div className={style['header-items']}>
-          <button className={style.item} onClick={() => navigate('/')}>Games</button>
-          <button className={style.item} onClick={() => navigate('projects')}>Projects</button>
-          <button className={style.item} onClick={() => navigate('about')}>About</button>
-        </div>
+        <div className = {style['header-items']}>{getButtons()}</div>
       </div>
     </header>
   )
